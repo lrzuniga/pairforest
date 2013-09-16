0aa23ecd32b426fcf2df3641276200b79ad61e2a
lrzuniga <lrzuniga@gmail.com>
2013-09-16 15:42:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pairforest::Application.config.secret_key_base = 'e4b4dad61b09d93e631eed44193bde1ad2c9682837c4c030770e4e26863d7d690b9d510a76f2a51d752c24088aca18c2af794b2ef05408dfd29cd385d25b5f5c'
