e07381aa4ee61008a8ca1c9fc704e2c5731ffee0
Michael van Holst <mikevanholst@gmail.com>
2013-09-16 19:24:29 -0400

    adds user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130916194823) do
+ActiveRecord::Schema.define(version: 20130916231946) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
