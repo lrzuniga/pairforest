d47997988111c53fd94f6c365645f1d30bd6282e
lrzuniga <lrzuniga@gmail.com>
2013-09-16 16:59:37 -0400

    Added Valid/Invalid tests for name, price as integer

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,19 +2,25 @@ require 'spec_helper'
 
   describe Product do
 
-   context "with a valid product" do
-
     let!(:product) {Product.create(:name => "Laptop", :description => "Crucial to developers", price_in_cents: 120099)}
+
+    context "Valid" do
     it "should have a valid factory" do
       product.should be_valid
-
+      end
     end
+    
+    context "Invalid" do
+        it "should be invalid with a non-integer price" do
+          product.price_in_cents = 14.5
+          product.should_not be_valid
+        end
 
-    it "should have a price in cents" do
-      product.price_in_cents = 14.5
-      product.should be_valid
+        it "should be invalid with no name" do
+          product.name = nil
+          product.should_not be_valid
+        end
     end
-
-  end
+  
 end
 
